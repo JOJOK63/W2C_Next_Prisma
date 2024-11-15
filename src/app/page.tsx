b218a4526e9e0d18c412be2e73dcf36c7382906e
JOJOK <krariajordan@gmail.com>
2024-11-15 12:15:19 +0100

    modification phone version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client"
-import { FaFacebookSquare, FaPhoneSquare, FaInstagramSquare } from "react-icons/fa";
+import { FaFacebookSquare, FaPhoneSquare, FaInstagramSquare,FaUserFriends,FaTruck  } from "react-icons/fa";
+import { LiaCookieSolid } from "react-icons/lia";
 import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
 
     function test(){
@@ -8,89 +11,99 @@ export default function Home() {
     }
     return(
       <>
-          <section className="">
-              <div className="">
-                  <Image  src="/products/cookies_fourres.jpg" alt="une image" width={200} height={100} className="m-auto"/>
+          <section className="pb-10">
+              <div>
+                  <Image  src="/products/cookies_fourres.jpg" alt="cookies" width={150} height={150} className="m-auto"/>
               </div>
-              <div className="w-[80%] m-auto">
-                  <h2 className="uppercase text-center mt-2.5 mb-2.5">mon histoire</h2>
-                  <p className="text-center">
+              <div className="w-[80%] m-auto ">
+                  <h2 className="uppercase font-bold text-center text-titleColor mt-6 mb-6">mon histoire</h2>
+                  <p className="text-center mt-6 mb-6">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos
                       quae praesentium repellat officia maxime deleniti placeat libero, tenetur
                       omnis magni accusantium est consequatur? Ab corrupti repudiandae similique
                       atque ipsa.
                   </p>
 
-                  <div className="flex justify-center mt-4">
-                      <button onClick={test} className="p-2 bg-buttonColor rounded-md">
-                          COMMANDER
+                  <div className="flex justify-center mt-6 mb-6">
+                      <button className="p-2 bg-buttonColor rounded-md text-sm ">
+                          <Link href="/products">
+                              COMMANDER
+                          </Link>
                       </button>
                   </div>
               </div>
               <div className="home_div_image">
-                  <Image src="/products/madeleines_brossard.jpg" alt="une image" width={50} height={50} className=""/>
+                  <Image src="/products/madeleines_brossard.jpg" alt="madeleines" width={150} height={150} className="m-auto"/>
               </div>
           </section>
 
-          <section className="flex flex-col justify-center items-center space-y-5 ">
-              <div className="home_div_image">
-                  <Image src="/logos/logo-W2C.png" alt="une image" width={50} height={50}/>
+          <section className="flex flex-col justify-center items-center space-y-10 bg-white pt-10 pb-10">
+              <div>
+                  <Image src="/products/sables_coeur.webp" alt="madeleines" width={150} height={150} className="m-auto"/>
               </div>
-                  {/* Icône Facebook */}
-                  <div className="flex flex-col justify-center items-center gap-y-1.5 hover:cursor-pointer">
-                      <a href="https://www.facebook.com/search/top?q=white%20cat%20cookies" target="_blank" rel="noopener noreferrer">
-                          <FaFacebookSquare className="text-iconColor hover:text-textColor fa-4x" size={40}/>
-                      </a>
+
+              <h2 className="uppercase font-bold text-center text-titleColor mt-6 mb-6">NOS RÉSEAUX</h2>
+
+              {/* Icône Facebook */}
+              <div className="flex flex-col justify-center items-center gap-y-1.5 hover:cursor-pointer">
+                  <a href="https://www.facebook.com/search/top?q=white%20cat%20cookies" target="_blank"
+                     rel="noopener noreferrer">
+                      <FaFacebookSquare className="text-iconColor hover:text-textColor fa-4x m-auto" size={40}/>
                       <p>White Cat Cookies</p>
-                  </div>
+                  </a>
+              </div>
 
-                  {/* Icône Téléphone */}
-                  <div className="flex flex-col justify-center items-center gap-y-1.5 hover:cursor-pointer">
-                      <FaPhoneSquare className="text-iconColor hover:text-textColor fa-4x" size={40}/>
-                      <p>06.20.62.07.69</p>
-                  </div>
+              {/* Icône Téléphone */}
+              <div className="flex flex-col justify-center items-center gap-y-1.5 ">
+                  <FaPhoneSquare className="text-iconColor fa-4x" size={40}/>
+                  <p>06.20.62.07.69</p>
+              </div>
 
-                  {/* Icône Instagram */}
-                  <div className="flex flex-col justify-center items-center gap-y-1.5 hover:cursor-pointer">
-                      <a href="https://www.instagram.com/white_cat_cookies/" target="_blank" rel="noopener noreferrer">
-                          <FaInstagramSquare className="text-iconColor hover:text-textColor fa-4x" size={40}/>
-                      </a>
+              {/* Icône Instagram */}
+              <div className="flex flex-col justify-center items-center gap-y-1.5 hover:cursor-pointer">
+                  <a href="https://www.instagram.com/white_cat_cookies/" target="_blank" rel="noopener noreferrer">
+                      <FaInstagramSquare className="text-iconColor hover:text-textColor fa-4x m-auto" size={40}/>
                       <p>white_cat_cookies</p>
-                  </div>
+                  </a>
+              </div>
               <div className="home_div_image">
-              <Image src="/logos/logo-W2C.png" alt="une image" width={50} height={50}/>
+                  <Image src="/products/minis_cookies.jpg" alt="madeleines" width={150} height={150} className="m-auto"/>
               </div>
           </section>
 
-          <section className="home_informations">
+          <section className="pt-10 pb-10">
               <div>
-                  <div className="home_informations_details">
-                      <i className="fa-solid fa-truck fa-6x"></i>
+                  <div className="text-center mb-6">
+                      <FaTruck className="m-auto mb-2"size={60}/>
                       <p>
                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
                           dignissimos quae praesentium repellat officia maxime deleniti placeat
-                          libero, tenetur
+                          libero, tenetur.
                       </p>
                   </div>
-                  <div className="home_informations_details">
-                      <i className="fa-solid fa-cookie-bite fa-6x"></i>
+                  <div className="mb-6">
+                      <LiaCookieSolid className="m-auto mb-2" size={60}/>
                       <p>
                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
                           dignissimos quae praesentium repellat officia maxime deleniti placeat
-                          libero, tenetur
+                          libero, tenetur.
                       </p>
                   </div>
-                  <div className="home_informations_details">
-                  <i className="fa-solid fa-people-line fa-6x"></i>
+                  <div className="mb-6">
+                      <FaUserFriends className="m-auto mb-2" size={60}/>
                       <p>
                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
                           dignissimos quae praesentium repellat officia maxime deleniti placeat
-                          libero, tenetur
+                          libero, tenetur.
                       </p>
                   </div>
-              </div>
-              <div>
-                  <button>NOUS CONTACTER</button>
+                  <div className="mb-6 text-center">
+                      <button className="p-2 bg-buttonColor rounded-md text-sm">
+                          <Link href="/products">
+                              CONTACTER
+                          </Link>
+                      </button>
+                  </div>
               </div>
           </section>
       </>
